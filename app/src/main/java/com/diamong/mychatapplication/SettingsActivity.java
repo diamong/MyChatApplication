3efbcd337271d56614a6f9b775271adba1266e81
diamong01 <45653307+diamong01@users.noreply.github.com>
2019-04-16 17:04:39 +0900

    버그 수정 43강 - 완성

--- app/src/main/java/com/diamong/mychatapplication/SettingsActivity.java
+++ app/src/main/java/com/diamong/mychatapplication/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -50,6 +51,8 @@ public class SettingsActivity extends AppCompatActivity {
     private StorageReference UserProfileImageRef;
     private ProgressDialog loadingBar;
 
+    private Toolbar SettingToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,6 +95,12 @@ public class SettingsActivity extends AppCompatActivity {
         userStatus = findViewById(R.id.set_profile_status);
         userProfileImage = findViewById(R.id.profile_image);
         loadingBar = new ProgressDialog(this);
+
+        SettingToolbar= findViewById(R.id.settings_toolbar);
+        setSupportActionBar(SettingToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setTitle(R.string.account_setting);
     }
 
     @Override
@@ -176,11 +185,11 @@ public class SettingsActivity extends AppCompatActivity {
         if (TextUtils.isEmpty(setStatus)) {
             Toast.makeText(this, getString(R.string.write_your_status), Toast.LENGTH_SHORT).show();
         } else {
-            HashMap<String, String> profileMap = new HashMap<>();
+            HashMap<String, Object> profileMap = new HashMap<>();
             profileMap.put("uid", currentUserID);
             profileMap.put("name", setUserName);
             profileMap.put("status", setStatus);
-            RootRef.child("Users").child(currentUserID).setValue(profileMap)
+            RootRef.child("Users").child(currentUserID).updateChildren(profileMap)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
